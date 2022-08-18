0f6cb4ec3def661baac695934e938aa7de377d85
GARAYEV-PG <rafkagarayev@gmail.com>
2022-08-18 17:17:13 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const myBtn = document.querySelector('button')
+const myBody = document.querySelector('body')
+
+let clicks = 0
+myBtn.addEventListener('click', addboxes)
+
+function addboxes(e) {
+    clicks++
+    e.preventDefault()
+
+    const addDiv = document.createElement('div')
+    myBody.appendChild(addDiv)
+    const addH2 = document.createElement('h2')
+    addDiv.appendChild(addH2)
+    if(clicks == 1) {
+        addDiv.classList.add('first_box')
+        addH2.innerText = 'Javascript'
+    }
+    if(clicks == 2) {
+        addDiv.classList.add('second_box')
+        addH2.innerText = 'is'
+    }
+    else if (clicks == 3)
+    {
+        addDiv.classList.add('third_box')
+        addH2.innerText = 'the'
+    }
+    else if (clicks == 4)
+    {
+        addDiv.classList.add('fourth_box')
+        addH2.innerText = 'best'
+    }
+    else if (clicks == 5)
+    {
+        addDiv.classList.add('fifth_box')
+        addH2.innerText = 'CSS'
+    }
+    else if (clicks == 6)
+    {
+        addDiv.classList.add('sixth_box')
+        addH2.innerText = 'is'
+    }
+    else if (clicks == 7)
+    {
+        addDiv.classList.add('seventh_box')
+        addH2.innerText = 'for'
+    }
+    else if (clicks == 8)
+    {
+        addDiv.classList.add('eighth_box')
+        addH2.innerText = 'noobs'
+    }
+    else if(clicks == 9)
+    {
+        const getDivs = document.querySelectorAll('div:not(.btn)')
+
+        for (let index = 0; index < getDivs.length; index++) {
+            getDivs[index].parentNode.removeChild(getDivs[index])
+            console.log(getDivs)
+            clicks = 0
+        }
+    }
+}
